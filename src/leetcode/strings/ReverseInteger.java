494bf25b82ad2cab10bfdca04a8e25b6efd669d7
Roman Roshchin <roschin.roman@gmail.com>
2021-09-04 18:23:18 -0700

    some more problems solved

---
+++ src/leetcode/strings/ReverseInteger.java
@@ -0,0 +1,52 @@
+package leetcode.strings;
+
+class ReverseInteger {
+
+    public static int reverseWithStr(int x) {
+        String reversed = new StringBuilder().append(Math.abs(x)).reverse().toString();
+        try {
+            return (x < 0) ? Integer.parseInt(reversed) * -1 : Integer.parseInt(reversed);
+        } catch (NumberFormatException e) {
+            return 0;
+        }
+    }
+
+    public static int reverse(int x) {
+        if (x < 10 && x > -10) {
+            return x;
+        }
+
+        boolean negative = x < 0;
+
+        char[] xStr = String.valueOf(x).toCharArray();
+        int start = negative ? 1 : 0;
+        int end = xStr.length - 1;
+        while (start < end) {
+            char tmp = xStr[start];
+            xStr[start] = xStr[end];
+            xStr[end] = tmp;
+            start++;
+            end--;
+        }
+        if (negative) {
+            xStr[0] = '0';
+        }
+        try {
+            int y = Integer.parseInt(new String(xStr));
+
+            if (negative) {
+                y *= -1;
+            }
+            return y;
+        } catch (NumberFormatException e) {
+            return 0;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(reverse(123));
+        System.out.println(reverse(-123));
+        System.out.println(reverse(120));
+        System.out.println(reverse(0));
+    }
+}
---
+++ src/leetcode/strings/ReverseString.java
@@ -0,0 +1,23 @@
+package leetcode.strings;
+
+import java.util.Arrays;
+
+class ReverseString {
+    public static void reverseString(char[] s) {
+        if (s.length == 0 || s.length == 1) {
+            return;
+        }
+        for (int i = 0; i < s.length / 2; i++) {
+            int j = s.length - 1 - i;
+            char tmp = s[i];
+            s[i] = s[j];
+            s[j] = tmp;
+        }
+    }
+
+    public static void main(String[] args) {
+        char[] s = { 'h', 'e', 'l', 'l', 'o' };
+        reverseString(s);
+        System.out.println(Arrays.toString(s));
+    }
+}
